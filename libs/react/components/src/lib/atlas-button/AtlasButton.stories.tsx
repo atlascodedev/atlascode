b27921a3f072a67e720a466c6a9ab1e9a8a81755
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-07-09 02:35:51 -0300

    [Feature] New material UI button variant (rounded) properly configured

--- libs/react/components/src/lib/atlas-button/AtlasButton.stories.tsx
+++ libs/react/components/src/lib/atlas-button/AtlasButton.stories.tsx
@@ -4,17 +4,24 @@ import { AtlasButton, AtlasButtonProps } from './AtlasButton';
 export default {
   component: AtlasButton,
   title: 'AtlasButton',
-  argTypes: {
-    variant: {
-      control: {
-        type: 'select',
-        options: ['contained', 'dashed', 'rounded', 'outlined', 'text'],
-      },
-    },
-  },
 } as Meta;
 
 const Template: Story<AtlasButtonProps> = (args) => <AtlasButton {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  children: 'Hello world',
+  disableElevation: true,
+} as AtlasButtonProps;
+Primary.parameters = {
+  controls: {
+    include: [
+      'color',
+      'children',
+      'disableElevation',
+      'disabled',
+      'size',
+      'variant',
+    ] as Array<keyof AtlasButtonProps>,
+  },
+};
--- libs/react/components/src/lib/atlas-button/AtlasButton.tsx
+++ libs/react/components/src/lib/atlas-button/AtlasButton.tsx
@@ -1,15 +1,70 @@
-import { Button, ButtonProps } from '@material-ui/core';
-import { SystemCssProperties, Theme } from '@material-ui/system';
+import { Button, ButtonProps, Box } from '@material-ui/core';
+import { SystemCssProperties, Theme, alpha } from '@material-ui/system';
 
-const roundedStyles: SystemCssProperties<Theme> = {
-  color: 'primary.main',
-};
+export type RoundedButtonProps = Omit<
+  ButtonProps,
+  'variant' | 'color' | 'children'
+>;
 
 /* eslint-disable-next-line */
-export interface AtlasButtonProps extends ButtonProps {}
+export interface AtlasButtonProps extends RoundedButtonProps {
+  color?: 'primary' | 'secondary';
+  variant?: 'outlined' | 'contained';
+  children?: React.ReactNode;
+}
 
-export function AtlasButton(props: AtlasButtonProps) {
-  return <Button {...props}>hello world</Button>;
+export function AtlasButton({
+  color = 'primary',
+  variant = 'contained',
+  children,
+  disableElevation = false,
+  ...props
+}: AtlasButtonProps) {
+  return (
+    <Box
+      {...props}
+      component={Button}
+      sx={{
+        transition: 'all 0.2s ease',
+        borderRadius: '15px',
+        px: '15px',
+        textTransform: 'inherit',
+        backgroundColor:
+          variant === 'contained' ? `${color}.main` : 'transparent',
+        color:
+          variant === 'contained' ? `${color}.contrastText` : `${color}.main`,
+        border: variant === 'outlined' ? `1px solid` : 'none',
+        borderColor:
+          variant === 'outlined' && !props.disabled
+            ? `${color}.main`
+            : 'transparent',
+        boxShadow: (theme) =>
+          disableElevation || variant === 'outlined'
+            ? theme.shadows[0]
+            : theme.shadows[3],
+        ':hover': {
+          backgroundColor: (theme) =>
+            variant === 'outlined'
+              ? alpha(theme.palette.primary.light, 0.1)
+              : `${color}.light`,
+          color:
+            variant === 'outlined' ? `${color}.main` : `${color}.contrastText`,
+          boxShadow: (theme) =>
+            disableElevation || variant === 'outlined'
+              ? theme.shadows[0]
+              : theme.shadows[1],
+        },
+        ':disabled': {
+          backgroundColor: (theme) => theme.palette.grey[200],
+          color: (theme) => theme.palette.grey[500],
+          boxShadow: (theme) => theme.shadows[0],
+          cursor: 'not-allowed',
+        },
+      }}
+    >
+      {children}
+    </Box>
+  );
 }
 
 export default AtlasButton;
