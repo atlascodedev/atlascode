df90d2b4391996b7413db4868cc3ebd9963004f3
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-07-17 00:02:48 -0300

    [CSS] Added responsive typography to hero screen white dots component

--- libs/react/src/lib/screens/hero-screen-white-dots/HeroScreenWhiteDots.tsx
+++ libs/react/src/lib/screens/hero-screen-white-dots/HeroScreenWhiteDots.tsx
@@ -1,16 +1,19 @@
-import { Box, colors } from '@material-ui/core';
+import { Box, colors, Typography } from '@material-ui/core';
+import { AtlasCSSVariant } from '../../utility/atlas-theme-provider/theme-utilities';
 
 /* eslint-disable-next-line */
 export interface HeroScreenWhiteDotsProps {
   patternColor?: string;
   patternSize?: string;
   backgroundColor?: string;
+  styleOverride?: AtlasCSSVariant;
 }
 
 export function HeroScreenWhiteDots({
   patternColor = '#bbbbbb60',
   patternSize = '0.8px',
   backgroundColor = '#fff',
+  styleOverride,
 }: HeroScreenWhiteDotsProps) {
   return (
     <Box
@@ -23,9 +26,38 @@ export function HeroScreenWhiteDots({
         gridTemplateRows: { xs: '50% 50%', md: 'none' },
         backgroundImage: `radial-gradient(${patternColor} ${patternSize}, ${backgroundColor} ${patternSize})`,
         backgroundSize: '10px 10px',
+        ...styleOverride,
       }}
     >
-      <Box></Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: { xs: 'flex-start' },
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            fontSize: { xs: '34px', md: '48px' },
+            fontWeight: 'black',
+            color: (theme) => theme.palette.primary.main,
+          }}
+        >
+          O futuro da sua carreira está em suas mãos.
+        </Typography>
+
+        <Typography
+          variant="h2"
+          sx={{
+            fontSize: { xs: '14px', md: '20px' },
+            color: (theme) => theme.palette.grey[600],
+          }}
+        >
+          Conheça os nossos cursos de pós-graduação e extensão em Medicina à
+          distância.
+        </Typography>
+      </Box>
 
       <Box></Box>
     </Box>
