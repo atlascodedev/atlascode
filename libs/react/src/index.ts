a224e1a95c4bfb8e441dbe8b766256cc6079d255
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-08-02 15:05:46 -0300

    [Feature] Initial implementation for image crossfade animation component

--- libs/react/src/index.ts
+++ libs/react/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/animations/image-crossfade/ImageCrossfade';
 export * from './lib/animations/fade-in-list/FadeInList';
 export * from './lib/screens/minimal-form-pic-over-blob/MinimalFormPicOverBlob';
 export * from './lib/forms/contact-form-dialog/ContactFormDialog';
---
+++ libs/react/src/lib/animations/image-crossfade/ImageCrossfade.tsx
@@ -0,0 +1,94 @@
+import { Box } from '@material-ui/core';
+import { AnimatePresence, AnimateSharedLayout } from 'framer-motion';
+import MotionBox from '../../utility/motion-box/MotionBox';
+
+/* eslint-disable-next-line */
+export interface ImageCrossfadeProps {
+  primaryImage: string;
+  secondaryImage: string;
+  swap?: boolean;
+}
+
+export function ImageCrossfade({
+  primaryImage,
+  secondaryImage,
+  swap,
+}: ImageCrossfadeProps) {
+  return (
+    <Box sx={{ width: '20rem', height: '20rem', position: 'relative' }}>
+      <AnimatePresence>
+        {!swap && primaryImage && (
+          <MotionBox
+            initial="visible"
+            exit="visible"
+            variants={{
+              hidden: {
+                opacity: 0,
+              },
+              visible: {
+                opacity: 1,
+              },
+            }}
+            sx={{
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            <Box
+              component="img"
+              src={primaryImage}
+              sx={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+              }}
+            />
+          </MotionBox>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {swap && secondaryImage && (
+          <MotionBox
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            variants={{
+              hidden: {
+                opacity: 0,
+              },
+              visible: {
+                opacity: 1,
+              },
+            }}
+            sx={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'contain',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+            }}
+          >
+            <Box
+              component="img"
+              src={secondaryImage}
+              sx={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+              }}
+            />
+          </MotionBox>
+        )}
+      </AnimatePresence>
+    </Box>
+  );
+}
+
+export default ImageCrossfade;
