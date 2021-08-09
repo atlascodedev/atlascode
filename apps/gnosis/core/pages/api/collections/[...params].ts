533b8c8b34b5d746252069de61973766cb0067cc
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-08-09 01:24:52 -0300

    [Refactor] Refactored API route endpoint with a better, slighly more secure design

--- apps/gnosis/core/pages/api/collections/[...params].ts
+++ apps/gnosis/core/pages/api/collections/[...params].ts
@@ -2,40 +2,43 @@ import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const API_BASE_URL = `${process.env.CLOUD_FUNCTION_BASE_URL}/collections/entries`;
+const API_SECRET = `${process.env.CLOUD_FUNCTION_API_KEY}`;
+
+const requestHasSecret = (queryObject: NextApiRequest['query']) =>
+  Object.keys(queryObject).includes('secret');
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method !== 'GET') {
-    console.log(req.method);
     return res.status(405).json({ error: 'Method not allowed' });
+  } else if (!requestHasSecret(req.query)) {
+    return res
+      .status(400)
+      .json({ error: 'Request did not include required secret' });
+  } else if (requestHasSecret(req.query) && req.query.secret !== API_SECRET) {
+    return res
+      .status(401)
+      .json({ error: 'Invalid API key, the request is unauthorized' });
   } else if (req.query.params.length < 1) {
     return res.status(400).json({ error: 'Invalid request format' });
   } else {
-    const params = req.query.params;
+    const params = req.query.params as string[];
 
     try {
-      switch (params.length) {
-        case 1: {   
-          const request = await axios.get(`${API_BASE_URL}/${params[0]}`);
+      const request = await axios.get(`${API_BASE_URL}/${params.join('/')}`);
 
-          return res.status(200).json({ data: request.data });
-        }
-        case 2: {
-          const request = await axios.get(
-            `${API_BASE_URL}/${params[0]}/${params[1]}`
-          );
-
-          return res.status(200).json({ data: request.data });
-        }
-
-        default: {
-          return res.status(400).json({ error: 'Invalid request' });
-        }
-      }
+      return res.status(200).json({
+        data: request.data,
+      });
     } catch (error) {
-      return res.status(500).json({ error: error.message });
+      return res.status(500).json({
+        error: {
+          code: error.code,
+          error: error.message,
+        },
+      });
     }
   }
 }
