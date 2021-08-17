fa80ee1f94f6ef27ab1feac136562c23ceb0d624
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-08-17 01:06:54 -0300

    [chore] removed merged libs from jest config projects array

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   projects: [
     '<rootDir>/apps/olympus/core',
-    '<rootDir>/libs/react/components',
     '<rootDir>/apps/gnosis/core',
-    '<rootDir>/apps/teste/core',
-    '<rootDir>/libs/react',
-    '<rootDir>/libs/react-web-hooks',
-    '<rootDir>/libs/jss-mixins',
-    '<rootDir>/libs/smooth-scrollbar-react-wrapper',
-    '<rootDir>/libs/mocks',
-    '<rootDir>/libs/react-web-sliders',
     '<rootDir>/libs/core',
   ],
 };
